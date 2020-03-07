204b8e25043e3112a902373bc86316b4503046db
Vitalya Zvyagin <78ku78@gmail.com>
2020-03-07 23:36:37 +0300

    dz8

--- src/ru/geekbrains/java/leve1/tictactoe/Map.java
+++ src/ru/geekbrains/java/leve1/tictactoe/Map.java
@@ -1,7 +1,253 @@
 package ru.geekbrains.java.leve1.tictactoe;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+public class Map extends JPanel {
+
+    public static final int MODE_H_V_A = 0;
+    public static final int MODE_H_V_H = 1;
+    private static final char PLAYER_DOT = 'X';
+    private static final char AI_DOT = 'O';
+    private static final char EMPTY_DOT = '.';
+    private static final int DELTA_DRAW = 10;
+    private static Random random = new Random();
+    private GameOverWindow gameOverWindow;
+
+    char[][] field;
+    int fieldSizeX;
+    int fieldSizeY;
+    int winLenght;
+    boolean gameOver;
+    boolean isWait;
+    int mode;
+    int cellHeight;
+    int cellWidth;
+    boolean isInitialized = false;
+    boolean stepPlayer2;
+
+    Map() {
+        setBackground(Color.blue);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                update(e);
+            }
+        });
+        gameOverWindow = new GameOverWindow();
+    }
+
+    void playerStep(int y,int x, char cymbol) {
+        setSym(y, x, cymbol);
+    }
+
+    void aiStep() {
+        //Ищкем выигрышный ход компьютера
+        for (int i = 0; i < fieldSizeY; i++)
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (isCellValid(i, j)) {
+                    setSym(i, j, AI_DOT);
+                    if (checkWin(AI_DOT)) return;
+                    setSym(i, j, EMPTY_DOT);
+                }
+            }
+        //Проверим игрока а нет ли у него будующего выигрошного хода
+        for (int i = 0; i < fieldSizeY; i++)
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (isCellValid(i, j)) {
+                    setSym(i, j, PLAYER_DOT);
+                    if (checkWin(PLAYER_DOT)) {
+                        setSym(i, j, AI_DOT);
+                        return;
+                    }
+                    setSym(i, j, EMPTY_DOT);
+                }
+            }
+        //Если ничего выигышного нет, то делаем как на уроке
+        int x;
+        int y;
+        do {
+            x = random.nextInt(fieldSizeX);
+            y = random.nextInt(fieldSizeY);
+        } while (!isCellValid(y, x));
+        setSym(y, x, AI_DOT);
+    }
+
+    void update(MouseEvent e) {
+        if (!gameOver && !isWait) {
+            int cellX = e.getX()/cellWidth;
+            int cellY = e.getY()/cellHeight;
+            if (mode == 0) modePlayAI(cellY, cellX);
+            else modeTwoPlayer(cellY, cellX);
+        }
+    }
+
+    // Режим двух игроков
+    void modeTwoPlayer(int y, int x) {
+        char cymbal =  stepPlayer2 ? AI_DOT : PLAYER_DOT;
+        playerStep(y,x,cymbal);
+        repaint();
+        if (checkWin(cymbal)) {
+            gameOverWindow.setMessage(stepPlayer2 ? "  Второй игрок выиграл!!" : "  Первый игрок выиграл!",this);
+            return;
+        }
+        if (isFuelFull()) {
+            gameOverWindow.setMessage("Ничья!",this);
+            return;
+        }
+        stepPlayer2 = !stepPlayer2;
+    }
+
+    // Режим против компьютера
+    void modePlayAI(int y, int x) {
+        playerStep(y,x,PLAYER_DOT);
+        repaint();
+        if (checkWin(PLAYER_DOT)) {
+            gameOverWindow.setMessage("  Игрок выиграл!",this);
+            return;
+        }
+        if (isFuelFull()) {
+            gameOverWindow.setMessage("  Нечья!",this);
+            return;
+        }
+        isWait = true;
+        aiStep();
+        isWait = false;
+        repaint();
+        if (checkWin(AI_DOT)) {
+            gameOverWindow.setMessage("  Искуственый интелект выиграл!",this);
+            return;
+        }
+        if (isFuelFull()) {
+            gameOverWindow.setMessage("DRAW!",this);
+            return;
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        render(g);
+    }
+
+    void startNewGame(int mode, int filedSizeX, int filedSizeY, int winLen) {
+        System.out.println(mode + " " + filedSizeX + " " + winLen);
+        gameOver = false;
+        stepPlayer2 = false;
+        isWait = false;
+        this.fieldSizeX = filedSizeX;
+        this.fieldSizeY = filedSizeY;
+        this.winLenght = winLen;
+        this.mode = mode;
+        field = new char[filedSizeY][filedSizeX];
+        initFields();
+        isInitialized = true;
+        repaint();
+    }
+
+    void render(Graphics g) {
+
+        if(!isInitialized) return;
+
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(3));
+
+        cellWidth = panelWidth/fieldSizeY;
+        cellHeight = panelHeight/fieldSizeX;
+
+        for (int i = 0; i < fieldSizeY; i++) {
+            int y = i * cellHeight;
+            g.drawLine(0, y, panelWidth, y);
+        }
+
+        for (int i = 0; i < fieldSizeX; i++) {
+            int x = i * cellWidth;
+            g.drawLine(x,0,x, panelHeight);
+        }
+
+        g.setColor(Color.DARK_GRAY);
 
 
-public class Map {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (field[j][i] != EMPTY_DOT) {
+                    if (field[j][i] == PLAYER_DOT) {
+                        // Рисуем крестик
+                        g.drawLine((i * cellWidth) + DELTA_DRAW, (j * cellHeight)+ DELTA_DRAW, (i + 1) * cellWidth - DELTA_DRAW, (j + 1) * cellHeight - DELTA_DRAW);
+                        g.drawLine((i + 1) * cellWidth - DELTA_DRAW, (j * cellHeight) + DELTA_DRAW , (i * cellWidth) + DELTA_DRAW, (j + 1) * cellHeight - DELTA_DRAW);
+                    }
+                    if (field[j][i] == AI_DOT) {
+                        // Рисуем нолик
+                        g.drawOval((i * cellWidth) + DELTA_DRAW, (j * cellHeight) + DELTA_DRAW, cellWidth - DELTA_DRAW * 2, cellHeight - DELTA_DRAW * 2);
+                    }
+                }
+            }
+        }
+    }
 
+    private void initFields() {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                field[i][j] = EMPTY_DOT;
+            }
+        }
+    }
+
+    private void setSym(int y, int x, char sym) {
+        field[y][x] = sym;
+    }
+
+    // проверка линии
+    private boolean checkLine(int y, int x, int vy, int vx, char sym) {
+        int wayX = x + (winLenght - 1) * vx;
+        int wayY = y + (winLenght - 1) * vy;
+        if (wayX < 0 || wayY < 0 || wayX > fieldSizeX - 1 || wayY > fieldSizeY - 1) return false;
+        for (int i = 0; i < winLenght; i++) {
+            int itemY = y + i * vy;
+            int itemX = x + i * vx;
+            if (field[itemY][itemX] != sym) return false;
+        }
+        return true;
+    }
+
+    private boolean checkWin(char sym) {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (checkLine(i, j, 0, 1,  sym)) return true;   // проверим линию по х
+                if (checkLine(i, j, 1, 1,  sym)) return true;   // проверим по диагонали х у
+                if (checkLine(i, j, 1, 0,  sym)) return true;   // проверим линию по у
+                if (checkLine(i, j, -1, 1, sym)) return true;  // проверим по диагонали х -у
+            }
+        }
+        return false;
+    }
+
+    boolean isFuelFull() {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (field[i][j] == EMPTY_DOT) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    boolean isCellValid(int y, int x) {
+        if (x < 0 || y < 0 || x > fieldSizeX - 1 || y > fieldSizeY - 1) {
+            return false;
+        }
+        return field[y][x] == EMPTY_DOT;
+    }
 }
+
--- src/ru/geekbrains/java/leve1/tictactoe/StartNewGameWindow.java
+++ src/ru/geekbrains/java/leve1/tictactoe/StartNewGameWindow.java
@@ -1,7 +1,112 @@
 package ru.geekbrains.java.leve1.tictactoe;
 
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StartNewGameWindow extends JFrame {
+
+    private final GameWindow gameWindow;
+    private static final int WIN_HEIGHT = 230;
+    private static final int WIN_WIDTH = 350;
+    private static final int MIN_WIN_LEN = 3;
+    private static final int MIN_FIELD_SIZE = 3;
+    private static final int MAX_WIN_LEN = 10;
+    private static final int MAX_FIELD_SIZE = 10;
+    private static final String STR_WIN_LEN = "Играть до: ";
+    private static final String STR_FILED_SIZE = "Размер поля: ";
+
+    private JRadioButton jrbHumVsAi = new JRadioButton("Человек vs Компьютер", true);
+    private JRadioButton jrbHumVsHum = new JRadioButton("Человек vs Человек");
+    private ButtonGroup gameMode = new ButtonGroup();
+
+    private JSlider slFieldSize;
+    private JSlider slWinLeght;
 
 
-public class StartNewGameWindow
+    public StartNewGameWindow(GameWindow gameWindow) {
+        this.gameWindow = gameWindow;
+        setTitle("Параметры новой игры!");
+        setSize(WIN_WIDTH, WIN_HEIGHT);
+        setLocationRelativeTo ( null );
 
-    {}
+        Rectangle gameWindowBounds = gameWindow.getBounds();
+        int posX = (int) (gameWindowBounds.getCenterX() - WIN_WIDTH/2);
+        int posY = (int) (gameWindowBounds.getCenterY() - WIN_HEIGHT/2);
+
+        setLocation(posX, posY);
+        setLayout(new GridLayout(10,1));
+
+        addGameControlsMode();
+        addGameControlsFieldWinLen();
+
+        JButton btnStartGame = new JButton("Начать игру");
+        btnStartGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnStartGame();
+            }
+        });
+        add(btnStartGame);
+
+    }
+
+    void btnStartGame() {
+        int gameMode;
+        if(jrbHumVsAi.isSelected()) {
+            gameMode = Map.MODE_H_V_A;
+        } else {
+            gameMode = Map.MODE_H_V_H;
+        }
+
+        int fieldSize = slFieldSize.getValue();
+        int winLen = slWinLeght.getValue();
+        gameWindow.startNewGame(gameMode, fieldSize, fieldSize, winLen);
+        setVisible(false);
+    }
+
+    private void addGameControlsMode() {
+        add(new JLabel("Выберите игровой режим"));
+        gameMode.add(jrbHumVsAi);
+        gameMode.add(jrbHumVsHum);
+        add(jrbHumVsAi);
+        add(jrbHumVsHum);
+    }
+
+    void addGameControlsFieldWinLen() {
+        add(new JLabel("Выберите размер поля"));
+        final JLabel lblFieldSize = new JLabel(STR_FILED_SIZE + MIN_FIELD_SIZE);
+        add(lblFieldSize);
+
+        slFieldSize = new JSlider(MIN_FIELD_SIZE, MAX_FIELD_SIZE, MIN_WIN_LEN);
+        slFieldSize.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                int currentFieldSize = slFieldSize.getValue();
+                lblFieldSize.setText(STR_FILED_SIZE + currentFieldSize);
+                slWinLeght.setMaximum(currentFieldSize);
+            }
+        });
+        add(slFieldSize);
+
+        add(new JLabel("Выберите победную линию:"));
+        final JLabel lblWinLen = new JLabel(STR_WIN_LEN + MIN_WIN_LEN);
+        add(lblWinLen);
+
+        slWinLeght = new JSlider(MIN_WIN_LEN, MAX_WIN_LEN, MIN_WIN_LEN);
+        slWinLeght.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                lblWinLen.setText(STR_WIN_LEN + slWinLeght.getValue());
+            }
+        });
+        add(slWinLeght);
+
+    }
+
+
+
+}
